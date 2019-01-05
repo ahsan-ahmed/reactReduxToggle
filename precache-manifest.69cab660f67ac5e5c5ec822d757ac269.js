98c20601a63721b9fd7f8c61489b46a4e66532c4
ahsan-ahmed <ahsan_ahmed01@yahoo.com>
2019-01-06 02:43:16 +0500

    Updates

---
+++ precache-manifest.69cab660f67ac5e5c5ec822d757ac269.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4cd998dc0d7d4544eef",
+    "url": "/reactReduxToggle/static/css/main.7a724a82.chunk.css"
+  },
+  {
+    "revision": "c4cd998dc0d7d4544eef",
+    "url": "/reactReduxToggle/static/js/main.c4cd998d.chunk.js"
+  },
+  {
+    "revision": "464cafb169160d83a2c5",
+    "url": "/reactReduxToggle/static/js/1.464cafb1.chunk.js"
+  },
+  {
+    "revision": "d976d2f944d4b1fb14d4",
+    "url": "/reactReduxToggle/static/js/runtime~main.d976d2f9.js"
+  },
+  {
+    "revision": "0545b4b2f646c8fbb0b91845c5ff4ff8",
+    "url": "/reactReduxToggle/index.html"
+  }
+];
